404d429c9576d913eac8794147f668767fcc02cb
Sven Kirschbaum <sven@kirschbaum.me>
2025-10-21 23:59:37 +0200

    feat: Automatically stop gapcloser if no discord channels are connected

--- src/main/java/de/elite12/musikbot/shared/ClientDTO.java
+++ src/main/java/de/elite12/musikbot/shared/ClientDTO.java
@@ -1,7 +1,18 @@
 package de.elite12.musikbot.shared;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import de.elite12.musikbot.shared.dtos.*;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, include = JsonTypeInfo.As.PROPERTY, property = "dtotype")
+@JsonSubTypes({
+        @JsonSubTypes.Type(NoListenerCommand.class),
+        @JsonSubTypes.Type(PauseCommand.class),
+        @JsonSubTypes.Type(ShutdownCommand.class),
+        @JsonSubTypes.Type(SongDTO.class),
+        @JsonSubTypes.Type(SongRequest.class),
+        @JsonSubTypes.Type(StopCommand.class),
+        @JsonSubTypes.Type(VolumeCommand.class),
+})
 public interface ClientDTO {
-    default String getDTOType() {
-        return this.getClass().getSimpleName();
-    }
 }
